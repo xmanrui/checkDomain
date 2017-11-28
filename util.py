d6791f66c5aba8d31c0a9f30692fadc474282fb1
xmanrui <841206367@qq.com>
2017-11-28 23:13:59 +0800

    增加读写配置的函数

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import configparser
 
 URL = 'http://www.aaw8.com/Api/DomainApi.aspx'
 
@@ -12,6 +13,7 @@ HEADERS = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,imag
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 
 DOMAIN = {'domain': ''}
+CONFIG_PATH = './config.conf'
 
 
 def is_available_domain(domain_name, prefix='', suffix='ai', domain_type='com'):
@@ -23,3 +25,22 @@ def is_available_domain(domain_name, prefix='', suffix='ai', domain_type='com'):
         return True
     else:
         return False
+
+
+def set_ai_suffix_domain_section(last_line_num):
+    cf = configparser.ConfigParser()
+    cf.read(CONFIG_PATH)
+    cf.set('ai_suffix_domain', 'last_line_num', str(last_line_num))
+    cf.write(open(CONFIG_PATH, 'w'))
+
+
+def get_ai_suffix_domain_last_line_num():
+    cf = configparser.ConfigParser()
+    cf.read(CONFIG_PATH)
+    return cf.getint('ai_suffix_domain', 'last_line_num')
+
+
+if __name__ == '__main__':
+    set_ai_suffix_domain_section(1000)
+    r = get_ai_suffix_domain_last_line_num()
+    print(r)
