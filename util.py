01095e36a615d4e056d0dbaf67cc53fadb270bcb
xmanrui <841206367@qq.com>
2017-11-29 09:07:09 +0800

    增加函数

--- util.py
+++ util.py
@@ -3,22 +3,41 @@ import requests
 import json
 import configparser
 
-URL = 'http://www.aaw8.com/Api/DomainApi.aspx'
-
+URL = 'http://www.aaw8.com/Api/DomainApi.aspx?domain='
 HEADERS = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
            'Accept-Encoding': 'gzip, deflate',
            'Accept-Language': 'zh-CN,zh;q=0.9',
            'Connection': 'Keep-Alive',
            'Host': 'www.aaw8.com',
+           'Cookie': 'JSESSIONID=RM566QD1-T8EQ2USZRI1Y6DCBQZAO1-6JWCSJAJ-L4QS; tmp0=c8WhVh5Avk6gEEWwyjscNwkaCPLez1M0UPVk%2BqMSEo62qaEiHvKSp0aCSRxRTEy2DREDCsbgmTcE%2BswxcJ8xJY8NMfqKteVQkGT0Zto6lGEQX0IpLbPcXATyt5hAdkkU132ibQs5E4dPtd6HKAVXJw%3D%3D',
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 
 DOMAIN = {'domain': ''}
 CONFIG_PATH = './config.conf'
 
 
+def is_available_domain_by_panda(domain_name, prefix='', suffix='ai', domain_type='com'):
+    domain = prefix + domain_name + suffix + '.' + domain_type
+    url = 'http://panda.www.net.cn/cgi-bin/check.cg?area_domain=' + domain
+
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate',
+               'Accept-Language': 'zh-CN,zh;q=0.9',
+               'Connection': 'Keep-Alive',
+               'Host': 'panda.www.net.cn',
+               'Upgrade-Insecure-Requests': '1',
+               'Cookie': 'JSESSIONID=RM566QD1-T8EQ2USZRI1Y6DCBQZAO1-6JWCSJAJ-L4QS; tmp0=c8WhVh5Avk6gEEWwyjscNwkaCPLez1M0UPVk%2BqMSEo62qaEiHvKSp0aCSRxRTEy2DREDCsbgmTcE%2BswxcJ8xJY8NMfqKteVQkGT0Zto6lGEQX0IpLbPcXATyt5hAdkkU132ibQs5E4dPtd6HKAVXJw%3D%3D',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    print(r.url)
+    soup = BeautifulSoup(r.text, 'xml')
+    t = soup.findAll('original')
+    print(t)
+
+
 def is_available_domain(domain_name, prefix='', suffix='ai', domain_type='com'):
-    DOMAIN['domain'] = prefix + domain_name + suffix + '.' + domain_type
-    r = requests.get(URL, params=DOMAIN, headers=HEADERS)
+    domain = prefix + domain_name + suffix + '.' + domain_type
+    r = requests.get(URL + domain, headers=HEADERS)
     soup = BeautifulSoup(r.text, 'html.parser')
     d = json.loads(soup.body.get_text())
     if d['StateID'] == 210:
@@ -48,6 +67,5 @@ def get_last_line_num(section):
     return cf.getint(section, 'last_line_num')
 
 if __name__ == '__main__':
-    set_ai_suffix_domain_section(1000)
-    r = get_ai_suffix_domain_last_line_num()
-    print(r)
+    t = is_available_domain('xieffdssffdsfsdffmanrui')
+    print(t)
