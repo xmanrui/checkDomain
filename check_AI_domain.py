d9d312291c513bf5fe3ad24fc79f6f6f5757bf59
xmanrui <841206367@qq.com>
2017-11-29 15:46:27 +0800

    增加功能：把已注册的域名保存到文件中

--- check_AI_domain.py
+++ check_AI_domain.py
@@ -1,20 +1,32 @@
 
 import time
 import random
-from util import is_available_domain, get_ai_suffix_domain_last_line_num, set_ai_suffix_domain_section
+from util import is_available_domain, get_ai_suffix_domain_last_line_num, \
+    set_ai_suffix_domain_section, set_last_line_num, get_last_line_num
 
 words_path = './niujing_eng_words.txt'
+mini_words_path = './mini_eng_dict.txt'
 available_ai_domain = './available_ai_domain.txt'
 mini_available_ai_domain = './mini_available_ai_domain.txt'
+mini_invalid_ai_domain = './mini_invalid_ai_domain.txt'
 
 
-def check_ai_suffix_domain():
+def check_ai_suffix_domain(eng_dict_path, valid_save_path, invalid_save_path, section, encoding='utf-8'):
+    """
+    从英文词典文件中读取单词，检测"单词ai.com"(比如cloudai.com)域名是否被注册，未被注册的域名保存到valid_save_path文件中
+    被注册的保存到invalid_save_path文件
+    :param eng_dict_path:文本英语词典
+    :param valid_save_path:
+    :param section: config.conf配置文件中保存的上一次从eng_dict_path读取到的行数(用于确定)
+    :param encoding: eng_dict_path文件的编码
+    :return:
+    """
     prefix = ''
     suffix = 'ai'
     domain_type = 'com'
-    last_line_num = get_ai_suffix_domain_last_line_num()  # 上一次运行最后从字典文件中读取的行数
+    last_line_num = get_last_line_num(section)  # 上一次运行最后从字典文件中读取的行数
 
-    with open(words_path, 'r', encoding='utf-8') as fh:
+    with open(eng_dict_path, 'r', encoding=encoding) as fh:
         count = 0
         for word in fh:
             count += 1
@@ -22,19 +34,26 @@ def check_ai_suffix_domain():
                 continue
 
             domain_name = word.strip()
-            try:
-                if is_available_domain(domain_name, prefix, suffix, domain_type):
-                    print(domain_name + prefix + suffix + '.' + domain_type)
-                    with open(available_ai_domain, 'a', encoding='utf-8') as out_fh:
-                        out_fh.writelines(domain_name + prefix + suffix + '.' + domain_type + '\n')
-                        out_fh.flush()
-                time.sleep(random.randint(1, 5))  # 延时
-            except Exception as e:
-                print(e)
-                time.sleep(10)
-                continue
+            for i in range(3):  # 最多尝试3次
+                try:
+                    if is_available_domain(domain_name, prefix, suffix, domain_type):
+                        with open(valid_save_path, 'a', encoding=encoding) as out_fh:
+                            print(domain_name + prefix + suffix + '.' + domain_type)
+                            out_fh.writelines(domain_name + prefix + suffix + '.' + domain_type + '\n')
+                            out_fh.flush()
+                    else:
+                        with open(invalid_save_path, 'a', encoding=encoding) as out_fh:
+                            print('invalid: ', domain_name + prefix + suffix + '.' + domain_type)
+                            out_fh.writelines(domain_name + prefix + suffix + '.' + domain_type + '\n')
+                            out_fh.flush()
+                    #time.sleep(random.randint(1, 5))  # 延时
+                    break
+                except Exception as e:
+                    print(e)
+                    time.sleep(10)
+                    continue
 
-            set_ai_suffix_domain_section(count)
+            set_last_line_num(section, count)
 
 if __name__ == '__main__':
-    check_ai_suffix_domain()
+    check_ai_suffix_domain(mini_words_path, mini_available_ai_domain, mini_invalid_ai_domain, 'ai_suffix_domain_from_mini_dict', 'gbk')
