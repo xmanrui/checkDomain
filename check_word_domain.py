1bde52bd33502b355409aec5e20a73e04b8a74e6
xmanrui <841206367@qq.com>
2017-12-02 17:01:49 +0800

    增加函数

--- check_word_domain.py
+++ check_word_domain.py
@@ -5,6 +5,7 @@ mini_words_path = './mini_eng_dict.txt'
 mini_available_word_domain_path = './mini_available_word_domain.txt'
 Oxford_words_path = './Oxford_eng_dict.txt'
 Oxford_available_word_domain_path = './Oxford_available_word_domain.txt'
+available_4char_com_cn_domain_path = './available_4char_com_cn_domain.txt'
 
 
 def check_words_domain(words_path, save_path, section, encoding='utf-8'):
@@ -69,7 +70,43 @@ def check_2char_3num_com_domain():
             print(e)
 
 
+def check_4char_com_cn_domain():
+    a = 97
+    z = 97 + 26
+    words = [chr(i)+chr(j)+chr(k)+chr(l) for i in range(a, z) for j in range(a, z) for k in range(a, z) for l in range(a, z)]
+    section = '4char_com_cn_domain'
+    last_num = get_last_line_num(section)
+    count = 0
+    domain_type = 'com.cn'
+    for word in words:
+        count += 1
+
+        if count <= last_num:
+            continue
+
+        for i in range(3):
+            try:
+                if is_available_domain(word.strip(), '', '', domain_type):
+                    print(word + '.' + domain_type)
+                    with open(available_4char_com_cn_domain_path, 'a', encoding='utf-8') as out_fh:
+                        out_fh.writelines(word.strip() + '.' + domain_type + '\n')
+                        out_fh.flush()
+                else:
+                    print('invalid: ', word.strip() + '.' + domain_type)
+                break
+            except Exception as e:
+                print(e)
+                time.sleep(10)
+                continue
+
+        try:
+            set_last_line_num(section, count)
+        except Exception as e:
+            print(e)
+
+
 if __name__ == '__main__':
     # check_domain(mini_words_path, mini_available_word_domain_path, 'gbk')
-    check_2char_3num_com_domain()
+    #check_2char_3num_com_domain()
+    check_4char_com_cn_domain()
 
