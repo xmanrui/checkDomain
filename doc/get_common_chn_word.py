9b3cfaa7675d564232ab19d9fcc546fb4853c804
xmanrui <841206367@qq.com>
2017-12-07 23:53:04 +0800

    增加函数用于获取单个汉字拼音

--- doc/get_common_chn_word.py
+++ doc/get_common_chn_word.py
@@ -9,6 +9,9 @@ src = './中文常用词汇.txt'
 dst = './中文常用词汇拼音.txt'
 src2 = './test2.txt'
 src3 = './test.txt'
+common_hanzi_pinyin = './common_hanzi_pinyin.txt'
+commont_3chars_hanzi_pinyin = './commont_3chars_hanzi_pinyin.txt'
+commont_2chars_hanzi_pinyin = './commont_2chars_hanzi_pinyin.txt'
 
 
 def foobar():
@@ -58,5 +61,46 @@ def foobar():
             fh.flush()
 
 
+def get_hanzi_pinyin_from_file(words_path):
+    py_set = set()
+    pinyin = Pinyin()
+    pattern = '[\u4e00-\u9fa5]'
+    p = re.compile(pattern)
+
+    with open(words_path, 'r', encoding='utf-8') as fh:
+        for line in fh:
+            r = p.findall(line)
+            if r:
+                for w in r:
+                    py = pinyin.get_pinyin(w, '')
+                    py_set.add(py)
+
+    return py_set
+
+
+def get_hanzi_pinyin():
+    r1 = get_hanzi_pinyin_from_file(src)
+    r2 = get_hanzi_pinyin_from_file(src2)
+    r3 = get_hanzi_pinyin_from_file(src3)
+
+    r = r1 | r2 | r3
+
+    with open(common_hanzi_pinyin, 'w', encoding='utf-8') as fh:
+        for w in r:
+            fh.writelines(w + '\n')
+            fh.flush()
+
+    with open(commont_3chars_hanzi_pinyin, 'w', encoding='utf-8') as fh:
+        for w in r:
+            if len(w) <= 3:
+                fh.writelines(w + '\n')
+                fh.flush()
+
+    with open(commont_2chars_hanzi_pinyin, 'w', encoding='utf-8') as fh:
+        for w in r:
+            if len(w) <= 2:
+                fh.writelines(w + '\n')
+                fh.flush()
+
 if __name__ == '__main__':
-    foobar()
+    get_hanzi_pinyin()
